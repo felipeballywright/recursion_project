1998b06f21b9fdb0bf227060a5c90dd6a92cf4ee
felipeballywright <felipeballywright@gmail.com>
2025-05-29 15:10:37 -0300

    052925

--- script.js
+++ script.js
@@ -1,17 +1,37 @@
-function fibs(){
-    const userInput = document.getElementById("user-input").value;
-    if(userInput > 20){
-        alert("Enter a number less than 20");
-        return
-    } else if(userInput < 2){
-        alert("Enter a number greater than 1");
-        return
-    }
-    const arr = [];
-    // for(let i = 0; arr.length < userInput + 1; ){
+console.log("Running script.js");
 
-    // }
+// ITERATION
+
+function fibs(fibArrLength){
+    const arr = [0, 1];
+    for(let i = 0; i < fibArrLength - 2; i++){
+        const newNumber = arr[arr.length - 1] + arr[arr.length - 2];
+        arr.push(newNumber);
+    }
+    console.log(arr);
 }
 
-const submitBtn = document.querySelector("#submit-button");
-submitBtn.addEventListener("click", fibs);
+// fibs(8);
+
+// RECURSION
+
+function fibsRec(numberOfRepetitions, arr){
+    if(!arr){
+        console.log("Creating arr from scratch.")
+        arr = [0, 1];
+        return fibsRec(numberOfRepetitions, arr);
+    }
+    const newNumber = arr[arr.length - 1] + arr[arr.length - 2];
+    arr.push(newNumber);
+    if(arr.length > numberOfRepetitions - 1){
+        console.log("The function reached the number of repetitions. The array is:", arr);
+        return arr
+    } else{
+        return fibsRec(numberOfRepetitions, arr);
+    }
+}
+
+// fibsRec(8);
+
+// MERGE SORT
+    
